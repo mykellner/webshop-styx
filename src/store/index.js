ec75bcf30f3f98247896b637a4764df5d24e3bd3
Ashish <ashisharyal64@gmail.com>
2020-12-08 10:38:17 +0100

    category index added in the product data

--- src/store/index.js
+++ src/store/index.js
@@ -7,45 +7,45 @@ export default new Vuex.Store({
   state: {
     products:[
       {
-        id:"1",
+        id:1,
         name:"Casa Marrone Appassimento",
         description:"Kryddig smak med fatkaraktär, inslag av körsbär, chokladpraliner, romrussin och vanilj.",
         price:"99",
         origin:"Italien",
         image:"",
-        category:"Red wine"
+        category:1
       },
       {
-        id:"2",
+        id:2,
         name:"Tommasi Adorato Appassionato",
         description:"Druvig smak med inslag av gula päron, mandel, vit persika och citrusskal.",
         price:"95",
         origin:"Italien",
         image:"",
-        category:"White wine"
+        category:2
       },
       {
-        id:"3",
+        id:3,
         name:"Castillo Monjardin",
         description:"Kryddig, fruktig smak med fatkaraktär, inslag av mörka körsbär, choklad, svarta vinbär, örter, plommon och kaffe.",
         price:"119",
         origin:"Spanien",
         image:"",
-        category:"Red wine"
+        category:1
         }
     ], 
     categories:[
       {
-        id:"1",
-        name:""        
+        id:1,
+        name:"Red wine"        
       },
       {
-        id:"2",
-        name:""        
+        id:2,
+        name:"White wine"        
       },
       {
-        id:"3",
-        name:""        
+        id:3,
+        name:"Category 3"        
       }
     ],   
     isAdmin:[],    
