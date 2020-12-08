4334c3b9670762d9c56f2bf7d81667acdc5302ed
Ashish <ashisharyal64@gmail.com>
2020-12-08 09:57:04 +0100

    setup data template for product and category information

--- src/store/index.js
+++ src/store/index.js
@@ -5,9 +5,51 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    products:[],
+    products:[
+      {
+        id:"1",
+        name:"",
+        description:"",
+        price:"",
+        origin:"",
+        image:"",
+        category:""
+      },
+      {
+        id:"2",
+        name:"",
+        description:"",
+        price:"",
+        origin:"",
+        image:"",
+        category:""
+      },
+      {
+        id:"3",
+        name:"",
+        description:"",
+        price:"",
+        origin:"",
+        image:"",
+        category:""
+      }
+    ], 
+    categories:[
+      {
+        id:"1",
+        name:""        
+      },
+      {
+        id:"2",
+        name:""        
+      },
+      {
+        id:"3",
+        name:""        
+      }
+    ],   
+    isAdmin:[],    
     cart:[],
-    isadmin:[],
   },
   mutations: {
   },
