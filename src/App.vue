e480f2054fbe2073edccfbd004cc9fb8676116ad
My <mykellner@gmail.com>
2020-12-08 10:34:47 +0100

    Added html to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div id="app">
     <div id="nav">
+      <header>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
     </div>
     <router-view/>
   </div>
