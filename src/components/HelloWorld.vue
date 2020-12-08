3786ce9a536b405456aef2f6246e5950f57d4e1f
Ashish <ashisharyal64@gmail.com>
2020-12-08 09:32:19 +0100

    store files minor update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="hello">
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,9 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    products:[],
+    cart:[],
+    isadmin:[],
   },
   mutations: {
   },
