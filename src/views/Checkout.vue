a39b4bf9fd281d00ba97bc93ce6c3fdc9f58fdf7
My <mykellner@gmail.com>
2020-12-10 14:40:34 +0100

    made a simple checkout form

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,3 +1,43 @@
 <template>
-    <h2>Checkout Page</h2>
+<div class="row d-flex checkout">
+    <div class="col-6">
+        <form>
+         <div class="form-group">
+            <label for="name">Name</label>
+            <input type="name" class="form-control" id="name" placeholder="Your name">
+         </div>
+         <div class="form-group">
+            <label for="email">Email address</label>
+            <input type="email" class="form-control" id="email" placeholder="Enter email">
+         </div>
+          <div class="form-group">
+            <label for="card-details">Card Details</label>
+            <input type="card-details" class="form-control" id="card-details" placeholder="Card details">
+         </div>
+         <button type="submit" class="btn btn-danger shop-button">Submit order</button>
+        </form>
+    </div>
+
+</div>
+
+    
 </template>
+
+<style scoped>
+
+.checkout {
+    justify-content: center;
+    padding-top: 20px;
+}
+
+
+.btn {
+      background-color: rgb(97, 37, 37);
+      border-color: white; 
+      width: 150px;
+      height: 50px;
+      font-size: 20px;
+  }
+
+
+</style>
